bfd1a7e90d7bdbcd0646ffdba0b1aa2a48ea9868
AdityaNagle <100685928+AdityaNagle@users.noreply.github.com>
2022-07-11 18:22:56 +0530

    Add files via upload

---
+++ dateServlet.java
@@ -0,0 +1,52 @@
+package com.aditya.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.Period;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+@WebServlet("/dateServlet")
+public class dateServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws NumberFormatException ,ServletException, IOException {
+		String dateString = null;
+		int age=0;
+		
+		try {
+	     dateString =request.getParameter("dob");
+		}catch(Exception e) {
+			
+							
+		}
+		LocalDate date = LocalDate.parse(dateString);
+		
+		date.getYear();
+		 LocalDate date2 = java.time.LocalDate.now();
+		 
+		age=date2.getYear()-date.getYear();
+		
+		Period p = Period.between (date, date2);
+		
+		System.out.println("Years (Difference) = "+p.getYears());
+	      System.out.println("Month (Difference) = "+p.getMonths());
+	      System.out.println("Days (Difference) = "+p.getDays());
+	      
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.write("<html><body>");
+		out.write("<h1> You are  "+age+" Years Old </h1>");
+		out.write("<h1> You are "+p.getYears()+" years "+p.getMonths()+" months "+p.getDays()+" days old</h1>");
+		out.write("</body></html>");
+	}
+
+	
+
+}
